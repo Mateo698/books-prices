8eee96dbd60bf8515f1d6d2f677368696ee6812d
Mateo Rada <mateora73@gmail.com>
2021-03-12 14:42:26 -0500

    It wasnt done lol. Now it can receive multiple cases continously

--- src/Main.java
+++ src/Main.java
@@ -11,43 +11,94 @@ public class Main {
 		int i = 0;
 		int j;
 		int M;
+		String output = "";
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
-		
-		int amount = Integer.parseInt(br.readLine());
-		j = amount - 1;
-		int[] array = new int[amount];
-		String[] stArray = br.readLine().split(" ");
-		for(int x=0;x<amount;x++) {
-			array[x] = Integer.parseInt(stArray[x]);
-		}
-		Arrays.sort(array);
-		M = Integer.parseInt(br.readLine());
-		M = M/2;
-		
-		boolean found = false;
-		while(i <= j && !found) {
-			int med = (i+j)/2;
-			if(array[med] == M) {
-				found = true;
-			}
-			else if(array[med] > M) {
-				j = med - 1;
+		boolean leave = false;
+		while(!leave) {
+			i=0;
+			j=0;
+			M=0;
+			
+			String firstEntry = br.readLine();
+			if(firstEntry.equals("")) {
+				firstEntry = br.readLine();
+				if(firstEntry.equals("")) {
+					leave = true;
+				}
+				else {
+					int amount = Integer.parseInt(firstEntry);
+					System.out.println(amount);
+					j = amount - 1;
+					int[] array = new int[amount];
+					String[] stArray = br.readLine().split(" ");
+					for(int x=0;x<amount;x++) {
+						array[x] = Integer.parseInt(stArray[x]);
+					}
+					Arrays.sort(array);
+					M = Integer.parseInt(br.readLine());
+					M = M/2;
+					
+					boolean found = false;
+					while(i <= j && !found) {
+						int med = (i+j)/2;
+						if(array[med] == M) {
+							found = true;
+						}
+						else if(array[med] > M) {
+							j = med - 1;
+						}
+						else {
+							i = med + 1;
+						}
+					}
+					if(array[i]>array[j]) {
+						int temp = i;
+						i = j;
+						j = temp;
+					}
+					output += "Peter should buy books whose prices are " + array[i] + " and " + array[j] + ".\n" ;
+				}
 			}
 			else {
-				i = med + 1;
+				int amount = Integer.parseInt(firstEntry);
+				System.out.println(amount);
+				j = amount - 1;
+				int[] array = new int[amount];
+				String[] stArray = br.readLine().split(" ");
+				for(int x=0;x<amount;x++) {
+					array[x] = Integer.parseInt(stArray[x]);
+				}
+				Arrays.sort(array);
+				M = Integer.parseInt(br.readLine());
+				M = M/2;
+				
+				boolean found = false;
+				while(i <= j && !found) {
+					int med = (i+j)/2;
+					if(array[med] == M) {
+						found = true;
+					}
+					else if(array[med] > M) {
+						j = med - 1;
+					}
+					else {
+						i = med + 1;
+					}
+				}
+				if(array[i]>array[j]) {
+					int temp = i;
+					i = j;
+					j = temp;
+				}
+				output += "Peter should buy books whose prices are " + array[i] + " and " + array[j] + ".\n" ;
 			}
 		}
-		if(array[i]>array[j]) {
-			int temp = i;
-			i = j;
-			j = temp;
-		}
-		bw.write("Peter should buy books whose prices are " + array[i] + " and " + array[j] + ".");
-		bw.flush();
 		
+		bw.write(output);
+		bw.flush();
 		br.close();
 		bw.close();
 	}
